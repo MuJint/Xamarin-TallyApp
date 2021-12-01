4f6429561ca118d6ae1b8f7cb652639d36a5b8e7
xiatingchan <xiatingchan@qq.com>
2021-12-01 17:24:18 +0800

    账单记录列表，优化滑动模块

---
+++ Tally.App/Controls/HistoryDetail.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tally.App.Helpers;
+using Tally.App.Models;
+using Tally.Framework.Enums;
+using Tally.Framework.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tally.App.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HistoryDetail : ContentPage
+    {
+        readonly string InComeColor = $"#24C389";
+        readonly string SpendColor = $"#3A3A62";
+        public HistoryDetail(List<SpendLog> spends)
+        {
+            InitializeComponent();
+            detailSource.ItemsSource = spends.OrderByDescending(s => s.DateTime).Select(s => new ExpenseRecord()
+            {
+                Icon = s.Icon.ToString(),
+                IconTitle = s.Icon.GetDescription(),
+                Description = s.Descrpition,
+                Rmb = s.Rmb.Value,
+                IsSpend = s.IsSpend == EnumSpend.Income ? "+" : "-",
+                TextColor = s.IsSpend == EnumSpend.Spend ? SpendColor : InComeColor,
+                Id = s.Id,
+                Time = $"{(s.DateTime >= DateTime.Now.AddDays(-3) ? s.DateTime.FormatDate() : s.DateTime.ToString("yyyy-MM-dd HH:mm:ss"))}"
+            });
+        }
+    }
+}
